d09fb283711cf2b3af11a1027aa2c287b02a4dae
Maksim S <devilhsss@gmail.com>
2026-01-16 14:06:38 +0300

    mmm

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,16 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:4000",
-    supportFile: "cypress/support/e2e.ts",
-    specPattern: "cypress/e2e/**/*.cy.{ts,tsx,js,jsx}",
-    setupNodeEvents(on, config) {},
-  },
-
-  component: {
-    devServer: {
-      framework: "react",
-      bundler: "webpack",
+    baseUrl: 'http://localhost:4000',
+    supportFile: 'cypress/support/e2e.ts',
+    specPattern: 'cypress/e2e/**/*.cy.{ts,tsx,js,jsx}',
+    setupNodeEvents(on, config) {
     },
   },
 });
--- src/components/ui/burger-constructor/burger-constructor.tsx
+++ src/components/ui/burger-constructor/burger-constructor.tsx
@@ -34,7 +34,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
         className={`${styles.noBuns} ${styles.noBunsTop} ml-8 mb-4 mr-5 text text_type_main-default`}
         data-testid='bun-first'
       >
-        Выберите булочку
+        Выберите булки
       </div>
     )}
     <ul className={styles.elements} data-testid='filling'>
@@ -72,7 +72,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
         className={`${styles.noBuns} ${styles.noBunsBottom} ml-8 mb-4 mr-5 text text_type_main-default`}
         data-testid='bun-second'
       >
-        Выберите булочку
+        Выберите булки
       </div>
     )}
     <div className={`${styles.total} mt-10 mr-4`}>
